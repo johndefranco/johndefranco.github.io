d1a8730d87e676b16bdd86bf7db8a3c0dd469038
johndefranco <qbk3xy@virginia.edu>
2023-11-22 17:28:29 -0500

    website is live

--- src/components/About/AboutParagraph.jsx
+++ src/components/About/AboutParagraph.jsx
@@ -4,7 +4,7 @@ function AboutParagraph() {
     return (
         <div className="flex items-center justify-center text-left">
             <p className="text-base max-w-md mb-6 md:text-xl max-w-l">
-                My name is John DeFranco, and I am a rising third-year Computer Science student in the University of Virginia's school of Engineering and Applied Sciences. I am currently seeking an internship for the summer of 2024, specifically in front-end software development. I have experience with Java, C, HTML/CSS, JavaScript, and multiple frameworks associated with those languages.
+                My name is John DeFranco, and I am a third-year Computer Science student in the University of Virginia's school of Engineering and Applied Sciences. I am currently seeking an internship for the summer of 2024, specifically in front-end software development. I have experience with Java, C, HTML/CSS, JavaScript, and multiple frameworks associated with those languages.
             </p>
         </div>
     )
